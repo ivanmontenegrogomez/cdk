ebda66f07413b7fa71f54611fb3a8300b8d7e1dd
ivanmontenegrogomez <montenegrogomezivan@gmail.com>
2023-10-11 09:21:58 -0300

    first commit

---
+++ bin/demoawspipeline.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { DemoawspipelineStack } from '../lib/demoawspipeline-stack';
+
+const app = new cdk.App();
+new DemoawspipelineStack(app, 'DemoawspipelineStack', {
+  env: { account: '482695813838', region: 'us-east-1' },
+  // comment
+});
---
+++ lib/demoawspipeline-stack.ts
@@ -0,0 +1,22 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { pipeline } from 'stream';
+
+export class DemoawspipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const democicdpipeline = new CodePipeline(this, 'demopipeline', 
+    {
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('Rahul1981/democicd021223', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ]
+      })
+    })
+ }
+}
